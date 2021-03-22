b99ce7d9753ec4726f69f2a217ca523091ea52ff
Taib Aboulkacem <56398779+taib1999@users.noreply.github.com>
2021-03-23 00:53:31 +0100

    Update Base de Données Code SQL.sql

--- Stage/Projet informatique 1/Base de Données Code SQL.sql
+++ Stage/Projet informatique 1/Base de Données Code SQL.sql
@@ -14,13 +14,14 @@ date_inscription_admin Timestamp NOT NULL
 #--table user pour les utilisateur
 CREATE TABLE controler_transitions.user( 
 id_user INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-id_academie_user varchar(255) NOT NULL,
+id_academie_user INT(11) NOT NULL,
 nom_prenom_user varchar(255) NOT NULL,
 metier_user varchar(255) NOT NULL,
 pass_user varchar(255) NOT NULL,
 email_user varchar(255) NOT NULL,
 ville_user varchar(255) NOT NULL,
 ecole_user varchar(255) NOT NULL,
+dommande varchar(255) NOT NULL DEFAULT "non",
 date_inscription_user Timestamp NOT NULL,
 Foreign key (id_academie_user) references admin_academie (id_admin)
 )ENGINE=MyISAM;
@@ -29,9 +30,9 @@ CREATE TABLE controler_transitions.dommande(
 id_dommande INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 id_user INT(11) NOT NULL,
 id_admin INT(11) NOT NULL,
-id_academie_rechercher varchar(255) NOT NULL,
-ville_rechercher varchar(255) NOT NULL,
-ecole_rechercher varchar(255) NOT NULL,
+id_academie_rechercher INT(11) NOT NULL,
+ville_rechercher varchar(255) NOT NULL DEFAULT "non-importe",
+ecole_rechercher varchar(255) NOT NULL DEFAULT "non-importe",
 date_dommande_user Timestamp NOT NULL,
 Foreign key (id_user) references user (id_user),
 Foreign key (id_admin) references user (id_academie_user),
@@ -81,8 +82,9 @@ Foreign key (envoi_a) references admin (id_user)
 #--table analyse system :
 CREATE TABLE controler_transitions.analyse_dommande(
 id_analyse INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-id_dommande INT(11) NOT NULL,
 id_user INT(11) NOT NULL,
+id_dommande_moi INT(11) NOT NULL,
+id_dommande_toi INT(11) NOT NULL,
 id_user_peut_etre INT(11) NOT NULL,
 id_admin_peut_etre INT(11) NOT NULL,
 dommande varchar(255) NOT NULL DEFAULT "non",
@@ -93,7 +95,8 @@ d_accord_admin_moi varchar(255) NOT NULL DEFAULT "non",
 date_daccord_ad_moi Timestamp NOT NULL,
 d_accord_admin_toi varchar(255) NOT NULL DEFAULT "non",
 date_daccord_ad_toi Timestamp NOT NULL,
-Foreign key (id_dommande) references dommande (id_dommande),
+Foreign key (id_dommande_moi) references dommande (id_dommande),
+Foreign key (id_dommande_toi) references dommande (id_dommande),
 Foreign key (id_user) references user (id_user),
 Foreign key (id_user_peut_etre) references user (id_user),
 Foreign key (id_admin_peut_etre) references admin_academie (id_admin)
